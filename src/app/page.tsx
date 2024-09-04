22f9108fa2edb42190ae772593d37335173ba22a
Harmanpreet Kaur <80103221+Harmanpawar15@users.noreply.github.com>
2024-09-04 17:10:43 -0400

    UI Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,12 @@ import { SpotlightPreview } from "@/components/Spotlight";
 import CountdownTimer from '../components/CountdownTimer';
 import { Cta } from "@/components/Cta";
 import { FeaturesTab } from "@/components/FeaturesTab";
+import Pricing from "@/components/Pricing";
 
 export default function Home() {
   return (
     <main className=" bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      {/* <div className="relative w-full flex flex-col items-center justify-center">
-        <Navbar />
-        <SpotlightPreview/>
-        <Cta />
-        <div className="mt-10 flex flex-col items-center">
-          <CountdownTimer />
-        </div>
-        <div className="w-full flex justify-center mt-16">
-          <FeaturesTab />
-        </div>
-      </div> */}
+      
       <div className="relative w-full flex flex-col items-center justify-center">
       <Navbar/>
       <SpotlightPreview/>
@@ -26,10 +17,13 @@ export default function Home() {
       <div className="mt-5 flex flex-col items-center">
           <CountdownTimer />
         </div>
-      {/* <CountdownTimer/> */}
+      
       <div className="w-full flex justify-center ">
           <FeaturesTab />
         </div>
+       
+        {/* <Pricing/> */}
+       
       </div>
     </main>
   );
--- src/components/FeaturesTab.tsx
+++ src/components/FeaturesTab.tsx
@@ -7,7 +7,7 @@ import pic2 from "../../public/assets/pic2.png";
 import pic3 from "../../public/assets/pic3.png";
 import pic4 from "../../public/assets/pic4.png";
 import pic5 from "../../public/assets/pic5.png";
-
+import { motion } from "framer-motion";
 export function FeaturesTab() {
   const tabs = [
     {
@@ -15,7 +15,7 @@ export function FeaturesTab() {
       value: "product",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-electric-cyan bg-gradient-to-br from-purple-700 to-violet-900">
-          <p className="mb-6">Product Tab</p>
+          <p className="mb-6">Home Screen</p>
           <DummyContent image={pic1} />
         </div>
       ),
@@ -25,17 +25,17 @@ export function FeaturesTab() {
       value: "services",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-electric-cyan bg-gradient-to-br from-purple-700 to-violet-900 ">
-          <p className="mb-6">Services Tab</p>
+          <p className="mb-6">Services </p>
           <DummyContent image={pic2} />
         </div>
       ),
     },
     {
-      title: "Playground",
+      title: "Cover Letter ",
       value: "playground",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-electric-cyan bg-gradient-to-br from-purple-700 to-violet-900">
-          <p className="mb-6">Playground Tab</p>
+          <p className="mb-6">Cover Letter Generation</p>
           <DummyContent image={pic3} />
         </div>
       ),
@@ -45,7 +45,7 @@ export function FeaturesTab() {
       value: "content",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-electric-cyan bg-gradient-to-br from-purple-700 to-violet-900">
-          <p className="mb-6">Content Tab</p>
+          <p className="mb-6">Customize Your CV</p>
           <DummyContent image={pic4} />
         </div>
       ),
@@ -55,7 +55,7 @@ export function FeaturesTab() {
       value: "random",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-electric-cyan bg-gradient-to-br from-purple-700 to-violet-900">
-          <p className="mb-6">Random Tab</p>
+          <p className="mb-6">Job Analysis</p>
           <DummyContent image={pic5} />
         </div>
       ),
@@ -63,8 +63,19 @@ export function FeaturesTab() {
   ];
 
   return (
-    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative flex flex-col max-w-5xl mx-auto w-full items-start justify-start my-40">
-      <Tabs tabs={tabs} />
+    <div className="  relative flex flex-col max-w-5xl  w-full items-centre justify-start my-40">
+     <motion.h2
+        className="text-4xl md:text-5xl font-bold text-center text-soft-white mb-24"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+       Discover What Sets Us Apart
+      </motion.h2>
+      <div className="w-full max-w-5xl h-[20rem] md:h-[40rem] [perspective:1000px]  mx-auto">
+        <Tabs tabs={tabs} />
+      </div>
+     
     </div>
   );
 }
@@ -81,5 +92,4 @@ const DummyContent = ({ image }: { image: StaticImageData }) => {
     />
   );
 };
-
 
